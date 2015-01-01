5b5ec0e28e74bd51b6f22677ef788dc513071f5b
anmatika <4ntti.matikainen@gmail.com>
2015-01-01 18:36:15 +0200

    modified:   client/app/scripts/controllers/cart.js 	modified:   client/app/template/ngCart/cart.html 	modified:   client/app/views/cart.html

--- client/app/scripts/controllers/cart.js
+++ client/app/scripts/controllers/cart.js
@@ -8,13 +8,14 @@
  * Controller of the meshopApp
  */
 angular.module('meshopApp')
-  .controller('CartCtrl', function ($scope) {
+  .controller('CartCtrl', ['$scope', 'ngCart', function ($scope, ngCart) {
     
     $scope.checkout = function() {
            console.log('checkout func');
-           $scope.summary = ngCart.toObject();
+           // $scope.summary = ngCart.toObject();
            
+           $scope.summary = ngCart.getItems();
          // Post your cart to your resource
          //$http.post('cart/', ngCart.toObject());
     }
-  });
+  }]);
