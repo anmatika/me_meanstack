88aef8b8adce42d736152aa5bc6d086a552a438e
anmatika <4ntti.matikainen@gmail.com>
2015-01-06 20:10:03 +0200

    modified:   client/app/index.html 	modified:   client/app/scripts/controllers/login.js 	new file:   client/app/scripts/services/loginSvc.js

--- client/app/scripts/controllers/login.js
+++ client/app/scripts/controllers/login.js
@@ -8,60 +8,47 @@
  * Controller of the meshopApp
  */
 angular.module('meshopApp')
-	.controller('LoginCtrl', function($scope, $http) {
+	.controller('LoginCtrl', function($scope, $http, loginSvc) {
 		$scope.send = function() {
-			console.log($scope.password);
+			loginSvc.send($scope.email, $scope.password)
+				.then(function(data) {
+						if (data.success) {
+							console.log('data.user._id: ' + data.user._id);
+							console.log('data.local.email: ' + data.user.local.email);
+							console.log('data.local.password: ' + data.user.local.password);
+							console.log('status: ' + status);
+							$scope.successmessage = "Login succeeded";
+							$scope.errormessage = "";
 
-			$http.post('/login', {
-				email: $scope.email,
-				password: $scope.password
-			}).
-			success(function(data, status, headers, config) {
-				if (data.success) {
-					console.log('data.user._id: ' + data.user._id);
-					console.log('data.local.email: ' + data.user.local.email);
-					console.log('data.local.password: ' + data.user.local.password);
-					console.log('status: ' + status);
-					$scope.successmessage = "Login succeeded";
-					$scope.errormessage = "";
-
-				} else {
-					$scope.errormessage = data.message;
-					$scope.successmessage = "";
-				} 
-				// this callback will be called asynchronously
-				// when the response is available
-			}).
-			error(function(data, status, headers, config) {
-				// called asynchronously if an error occurs
-				// or server returns response with an error status.
-				console.log(data);
-			});
+						} else {
+							$scope.errormessage = data.message;
+							$scope.successmessage = "";
+						}
+						// this callback will be called asynchronously
+						// when the response is available	
+					},
+					function(reason) {
+						console.log(reason);
+					});
 		};
 
-		$scope.isLoggedIn = function () {
-			$http.get('/isloggedin').
-			success(function(data, status, headers, config) {
-				console.log(data);
-				// this callback will be called asynchronously
-				// when the response is available
-			}).
-			error(function(data, status, headers, config) {
-				// called asynchronously if an error occurs
-				// or server returns response with an error status.
-			});
+		$scope.isLoggedIn = function() {
+			loginSvc.isLoggedIn()
+				.then(function(data) {
+						console.log(data);
+					},
+					function(reason) {
+						console.log(reason);	
+					});
 		};
 
-		$scope.logout = function () {
-			$http.get('/logout').
-			success(function(data, status, headers, config) {
-				console.log(data);
-				// this callback will be called asynchronously
-				// when the response is available
-			}).
-			error(function(data, status, headers, config) {
-				// called asynchronously if an error occurs
-				// or server returns response with an error status.
-			});
+		$scope.logout = function() {
+			loginSvc.logout()
+			 .then(function(data){
+			 	console.log(data);			
+			 },
+			 function(reason){
+			 	console.log(reason);	
+			 });
 		};
 	});
---
+++ client/app/scripts/services/loginSvc.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name meshopApp.loginSvc
+ * @description
+ * # loginSvc
+ * Service in the meshopApp.
+ */
+angular.module('meshopApp')
+  .service('loginSvc', function($http, $q) {
+
+    return {
+
+      send: function(email, password) {
+        
+        var deferred = $q.defer();
+        console.log(password);
+
+        $http.post('/login', {
+          email: email,
+          password: password
+        }).
+        success(function(data, status, headers, config) {
+          deferred.resolve(data);
+        }).
+        error(function(data, status, headers, config) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          console.log(data);
+          deferred.reject(data);
+
+        });
+
+        return deferred.promise;
+
+      },
+
+      isLoggedIn: function() {
+        var deferred = $q.defer();
+        
+        $http.get('/isloggedin').
+        success(function(data, status, headers, config) {
+          deferred.resolve(data);
+          // this callback will be called asynchronously
+          // when the response is available
+        }).
+        error(function(data, status, headers, config) {
+          deferred.reject(data);
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+
+        return deferred.promise;
+      },
+
+      logout: function() {
+        var deferred = $q.defer();
+
+        $http.get('/logout').
+        success(function(data, status, headers, config) {
+          deferred.resolve(data);
+          // this callback will be called asynchronously
+          // when the response is available
+        }).
+        error(function(data, status, headers, config) {
+          deferred.reject(data);
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+
+        return deferred.promise;
+      }
+    }
+  });
